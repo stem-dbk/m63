006caa58ad69791cd0123390d3ddde5351827c2c
crearth <46131286+crearth@users.noreply.github.com>
2021-03-01 11:40:13 +0100

    flow

---
+++ arduino/flow/flow.ino
@@ -0,0 +1,13 @@
+draai omlaag 35
+volgens tijd vloeiend stoppen
+tot snelheid 10
+
+als eind1 = 1
+stop volledig vloeiend
+start vloeiend draaien omhoog
+volgens tijd vloeiend stoppen
+tot snelheid 15
+
+als eind 2 = 1
+stop volledig vloeiend
+start vloeiend omlaag
