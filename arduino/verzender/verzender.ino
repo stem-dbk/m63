9a86df5dcbe6e702a93b8a1f8be901ff43086362
Arthur <46131286+crearth@users.noreply.github.com>
2021-05-16 22:43:37 +0200

    Create verzender.ino

---
+++ arduino/verzender/verzender.ino
@@ -0,0 +1,28 @@
+/*
+  433 MHz RF Module Transmitter Demonstration 1
+  RF-Xmit-Demo-1.ino
+  Demonstrates 433 MHz RF Transmitter Module
+  Use with Receiver Demonstration 1
+*/
+ 
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+ 
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+ 
+void setup()
+{
+    // Initialize ASK Object
+    rf_driver.init();
+}
+ 
+void loop()
+{
+    const char *msg = "Test van de verzender!";
+    rf_driver.send((uint8_t *)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+    delay(1000);
+}
