a7fd60eea756707b90cfbf9306afca44a9fc79f9
Arthur <46131286+crearth@users.noreply.github.com>
2021-06-10 19:23:33 +0200

    Update vloei.ino

--- arduino/vloei/vloei.ino
+++ arduino/vloei/vloei.ino
@@ -151,22 +151,6 @@ int vloeiStop(int remSnelheid, int remStand, int remTijd){
   }
 }
 
-// een functie voor aanpasbare snelheid
-int potentio(){
-  int potentioVal = analogRead(potentioSen);
-  int rangeVal = 512/35;
-  if(potentioVal < 512){
-    snelOmlaag = 35 - (-(potentioVal-512)/rangeVal);
-    snelOmhoog = 150 - (-(potentioVal-512)/rangeVal);
-  } else if (potentioVal > 512){
-    snelOmlaag = 35 + (potentioVal-512)/rangeVal;
-    snelOmhoog = 150 + (-(potentioVal-512)/rangeVal);
-  } else if(potentioVal = 512){
-    snelOmlaag = 35;
-    snelOmhoog = 150;
-  }
-}
-
 int potentioOmlaag(){
   int potentioVal = analogRead(potentioSen);
   int rangeVal = 512/35;
